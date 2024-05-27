e4495b5b31530475ab7d669da55ae65dd88364ca
Sheik Althaf <althafmech@outlook.com>
2024-05-27 22:38:31 +0530

    Auto-generated commit

--- chunk-42KYJ22C.js
+++
@@ -1,1 +0,0 @@
-import{ca as i}from"./chunk-H5GOFPZC.js";var p=(()=>{let r=class r{transform(e,...a){return Array.from({length:e},(o,n)=>n+1)}};r.\u0275fac=function(a){return new(a||r)},r.\u0275pipe=i({name:"range",type:r,pure:!0,standalone:!0});let t=r;return t})();export{p as a};

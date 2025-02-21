94f44f6478a14b27920d564838af8cddff65b598
Sheik Althaf <althafmech@outlook.com>
2025-02-21 13:01:22 +0530

    docs: update the example for virtulizer

--- projects/docs/src/app/ui/virtualizer.component.ts
+++ projects/docs/src/app/ui/virtualizer.component.ts
@@ -8,11 +8,12 @@ import { VirtualFor, Virtualizer } from '@meeui/adk/virtualizer';
   imports: [Virtualizer, VirtualFor, Button, Heading],
   template: `
     <div>
-      <button meeButton (click)="vv.scrollToIndex(0)">Scroll to 0</button>
-      <button meeButton (click)="vv.scrollToIndex(10)">Scroll to 10</button>
-      <button meeButton (click)="vv.scrollToIndex(99999)">Scroll to 99999</button>
-
-      <div class="grid grid-cols-2 gap-4">
+      <div class="flex gap-2">
+        <button meeButton (click)="vv.scrollToIndex(0)">Scroll to 0</button>
+        <button meeButton (click)="vv.scrollToIndex(10)">Scroll to 10</button>
+        <button meeButton (click)="vv.scrollToIndex(99999)">Scroll to 99999</button>
+      </div>
+      <div class="grid grid-cols-1 gap-4 lg:grid-cols-2">
         <div>
           <h4 meeHeader="sm" class="mt-4">Vertical</h4>
           <mee-virtualizer #vv [itemSize]="50" class="h-[400px] border">
@@ -20,14 +21,15 @@ import { VirtualFor, Virtualizer } from '@meeui/adk/virtualizer';
               *virtualFor="let item of items; let i = index"
               class="flex h-full items-center border-b px-b4"
             >
-              Row {{ i }}
+              id: {{ item.id }} | name: {{ item.name }} | description: {{ item.description }} |
+              value: {{ item.value }}
             </div>
           </mee-virtualizer>
         </div>
 
         <div>
           <h4 meeHeader="sm" class="mt-4">Horizontal</h4>
-          <mee-virtualizer #vv orientation="horizontal" [itemSize]="50" class="h-[400px] border">
+          <mee-virtualizer orientation="horizontal" [itemSize]="50" class="h-[400px] border">
             <div
               *virtualFor="let item of items; let i = index"
               class="flex h-full w-full items-center border-r py-b4 [writing-mode:vertical-lr]"
@@ -41,7 +43,12 @@ import { VirtualFor, Virtualizer } from '@meeui/adk/virtualizer';
   `,
 })
 export default class VirtualizerComponent {
-  items = Array.from({ length: 100000 }).map((_, i) => i);
+  items = Array.from({ length: 100000 }).map((_, i) => ({
+    id: i,
+    name: `Item ${i}`,
+    description: `Description ${i}`,
+    value: i,
+  }));
   constructor() {
     console.log('this.items');
   }
